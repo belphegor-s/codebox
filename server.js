2c2c11bc4154ce7aef88a2c0697a03daa456806b
Ayush Sharma <ayush2162020@gmail.com>
2023-05-04 11:12:58 +0530

    refactor: testing serverless function

--- server.js
+++ server.js
@@ -66,4 +66,4 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-module.exports = server;
+module.exports = app;
